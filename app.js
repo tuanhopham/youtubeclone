783a91747b353c61420916116db8cd3668ee0dac
phạm tuấn <minhtuanpham2111@gmail.com>
2022-10-06 12:10:18 +0700

    pc model2

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const menu = document.querySelector('.menu')
 const sidebar = document.querySelector('.sidebar')
 const sidebarMin = document.querySelector('.sidebar-min')
 const ScrollMenu = document.querySelector('.scroll-menu')
-
+const content = document.querySelector('.content')
 menu.onclick = function(){
    sidebar.classList.toggle('show')
    sidebarMin.classList.toggle('show')
+   content.classList.toggle('ninety-five')
 }
 ScrollMenu.onwheel = function(e){
    
